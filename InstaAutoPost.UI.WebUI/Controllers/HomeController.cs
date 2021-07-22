0e9306fd4503583df9215be4b4340f1f673ac266
Mehmet KOYUNCU <mehmet.koyuncu@bilgeadam.com>
2021-07-23 00:38:52 +0300

    Edit.

---
+++ InstaAutoPost.UI.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿
+using InstaAutoPost.UI.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace InstaAutoPost.UI.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var client = new RestClient("https://localhost:44338/source/getallsources");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Name", "\"Deneme\"");
+            IRestResponse response = client.Execute(request);
+            var weatherForecast = JsonSerializer.Deserialize<SourceModel>(response.Content);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ InstaAutoPost.UI.WebUI/Models/SourceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaAutoPost.UI.WebUI.Models
+{
+    public class SourceModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string Image { get; set; }
+    }
+}

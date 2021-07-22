77ab45e8b808d08f62d0df8a2e433d01a8a96c5a
Mehmet KOYUNCU <mehmet.koyuncu@bilgeadam.com>
2021-07-22 15:58:47 +0300

    UnitOfWork added.

--- InstaAutoPost.RSSService.Data/Context/RSSContextEF.cs
+++ InstaAutoPost.RSSService.Data/Context/RSSContextEF.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore.Proxies;
 
 
 namespace InstaAutoPost.RSSService.Data.Context
@@ -12,9 +13,11 @@ namespace InstaAutoPost.RSSService.Data.Context
         public DbSet<Image> Images { get; set; }
         public DbSet<Source> Sources { get; set; }
         public DbSet<SourceContent> SourceContents { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseLazyLoadingProxies();
             optionsBuilder.UseSqlServer(@"Data Source=TCBLGADMCONS023;Initial Catalog=InstaAutoPostRssDb;Integrated Security=SSPI;");
         }
     }
---
+++ InstaAutoPost.RSSService.Data/UnitOfWork/Abstract/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using InstaAutoPost.RSSService.Data.Entities.Abstract;
+using InstaAutoPost.RSSService.Data.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaAutoPost.RSSService.Data.UnitOfWork.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : EntityBase, IEntity, new();
+        int SaveChanges();
+    }
+}
